709f8ae494a982069544ae23337a04e5eb1eb6cc
Samet <gsametdogan@gmail.com>
2023-08-22 12:28:51 +0300

    Initial Commit

---
+++ Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
